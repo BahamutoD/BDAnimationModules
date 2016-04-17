6f4f82db940cf280846d2e30a1ff9c3abf8ac6a2
Jesus Rodriguez <jesus.rodriguez.valencia@gmail.com>
2016-04-17 18:31:10 +0200

    Initial compatibility with 1.1

--- BDAnimationModules/MouseAimLight.cs
+++ BDAnimationModules/MouseAimLight.cs
@@ -92,7 +92,7 @@ namespace BDAnimationModules
 				deployStates = SetUpAnimation(deployAnimName, this.part);
 			}
 
-			emissiveMatRef = part.FindModelTransform(emissiveObjectName).renderer.material;
+			emissiveMatRef = part.FindModelTransform(emissiveObjectName).GetComponent<Renderer>().material;
 			
 			spotLight = part.FindModelTransform(lightObjectName).GetComponent<Light>();
 			spotlightIntensity = spotLight.intensity;
